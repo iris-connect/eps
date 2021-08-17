6238646d35931b4e35382a63b56b2dee7226f072
Andreas Dewes <andreas.dewes@kiprotect.com>
2021-08-17 16:35:53 +0200

    Added copyright headers.

--- datastores/file.go
+++ datastores/file.go
@@ -1,7 +1,7 @@
 // IRIS Endpoint-Server (EPS)
 // Copyright (C) 2021-2021 The IRIS Endpoint-Server Authors (see AUTHORS.md)
 //
-// This program is free software: you can filetribute it and/or modify
+// This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as
 // published by the Free Software Foundation, either version 3 of the
 // License, or (at your option) any later version.
--- datastores/redis.go
+++ datastores/redis.go
@@ -1,3 +1,19 @@
+// IRIS Endpoint-Server (EPS)
+// Copyright (C) 2021-2021 The IRIS Endpoint-Server Authors (see AUTHORS.md)
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 package datastores
 
 import (
@@ -153,18 +169,3 @@ func (d *Redis) Open() error {
 func (d *Redis) Close() error {
 	return d.client.Close()
 }
-
-/*
-#	result, err := r.db.Client().HGetAll(string(r.fullKey)).Result()
-	if err != nil {
-		if err == redis.Nil {
-			return nil, NotFound
-		}
-		return nil, err
-	}
-	byteMap := map[string][]byte{}
-	for k, v := range result {
-		byteMap[k] = []byte(v)
-	}
-	return byteMap, nil
-*/
