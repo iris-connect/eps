eae78be48742c035ecebdc71bfe3a315c1c75823
Jens Kutzsche <github@gebea.de>
2021-09-24 16:30:26 +0200

    fix: extends the folders for chown and make this configurable

--- .scripts/entrypoint-proxy.sh
+++ .scripts/entrypoint-proxy.sh
@@ -1,7 +1,30 @@
 #!/bin/sh
 
-if ! su iris -c "find ./settings -type f -exec cat {} > /dev/null +"; then
-    chown -R iris:iris ./settings
+if [[ -z "${MOUNT_POINTS}" ]]
+then
+    echo "The environment variable MOUNT_POINTS can be set to a comma separated list of the mount points in the container. Use default now."
+    MOUNT_POINTS="/config,/tls,/app/settings,/storage,/app/db,/tmp"
 fi
+
+echo "MOUNT_POINTS is set to \"${MOUNT_POINTS}\""
+
+for point in ${MOUNT_POINTS//,/ }
+do
+    if [ ! -d ${point} ]
+    then
+        echo "Skip non-existent directory: \"${point}\""
+        continue
+    fi
+
+    if ! su iris -c "find ${point} -type f -exec cat {} > /dev/null +"
+    then
+        echo "chown is made for \"${point}\""
+        chown -R iris:iris ${point}
+    else
+        echo "Skip readable directory: \"${point}\""
+    fi
+done
+
+echo "Execute proxy with user iris"
 
 exec su iris -c "./proxy $*"
